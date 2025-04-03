1357c115ceb7164f6bd3663df0e759646628958a
Your Name <okedijimicheal1@gmail.com>
2025-04-03 01:34:45 -0700

    add environment configuration and initial project setup

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+import './App.css'
+
+function App() {
+  const url = 'http://localhost:5500/test'
+  const getInfo = ( )=>{
+    axios.get(url)
+    .then((res)=>{
+      console.log(res.data);
+      
+    })
+    .catch((err)=>{
+      console.log(err);
+      
+    })
+  }
+
+
+  return (
+    <>
+      <button onClick={getInfo}>Get Info</button>
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,17 @@
+// const { config, configDotenv } = require('dotenv');
+const express = require ('express');
+const app = express();
+const dotenv = require('dotenv').config();
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 5300;
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json());
+app.use (bodyParser.urlencoded({extended: true}))
+app.get('/test', (req , res )=>{
+    res.send({name: 'femi'})
+})
+app.listen(port, ()=>{`Server is running on port ${port}`})
+console.log(`Server is running on port ${port}`);
+
