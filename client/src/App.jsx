24967569a26fd99983a59a8989bc1cf5b2845653
Your Name <okedijimicheal1@gmail.com>
2025-04-03 01:46:32 -0700

    update API endpoint to use Vercel deployment URL

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,8 @@ import axios from 'axios'
 import './App.css'
 
 function App() {
-  const url = 'http://localhost:5500/test'
+  // const url = 'http://localhost:5500/test'
+  const url = 'https://second-class.vercel.app/api'
   const getInfo = ( )=>{
     axios.get(url)
     .then((res)=>{
