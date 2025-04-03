98e64621d86a19e2aec3139f5f145fbdde205439
Your Name <okedijimicheal1@gmail.com>
2025-04-03 01:49:26 -0700

    update test endpoint to /api for consistency with API structure

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const cors = require('cors')
 app.use(cors())
 app.use(express.json());
 app.use (bodyParser.urlencoded({extended: true}))
-app.get('/test', (req , res )=>{
+app.get('/api', (req , res )=>{
     res.send({name: 'femi'})
 })
 app.listen(port, ()=>{`Server is running on port ${port}`})
